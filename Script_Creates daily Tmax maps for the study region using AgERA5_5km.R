8ce159ccb0c6e6e46afc7dc61761769b260172f1
Camilo Barrios-Pérez <c.barrios@cgiar.org>
2025-11-13 11:01:33 -0500

    Create Script_Creates daily Tmax maps for the study region using AgERA5_5km.R
    Creates daily Tmax maps for the study region using AgERA5_5km

---
+++ Script_Creates daily Tmax maps for the study region using AgERA5_5km.R
@@ -0,0 +1,76 @@
+#####################################################################
+## This script generates daily Tmax maps for the study-area extent ##
+## using AgERA5_V2 data for 1981-2025.                             ##
+## Created by: Camilo Barrios-Perez (Ph.D) - c.barrios@cgiar.org   ##
+#####################################################################
+
+######################
+## Upload libraries ##
+######################
+
+library(terra) # Para manejo de rásteres y análisis espacial
+library(sp)    # Para trabajar con objetos espaciales
+library(sf)    # Para leer y manipular datos vectoriales (shapefiles)
+
+##############################################################
+## Import the shapefile of the study region in the Amazonia ##
+##############################################################
+
+Zoi = st_read("\\\\Catalogue/sat_pma/data/inputs/zoi/zoi_geo.shp")
+
+########################################################
+## Import the reference raster file at 5km resolution ##
+########################################################
+
+reference.5km = terra::rast("\\\\Catalogue/sat_pma/data/raw/Clima_Fuentes_Secundarias/Diario/CHIRPS_V3_ERA5/1981/chirps-v3.0.1981.01.01.tif")
+
+#################################################################################################
+### This script section will make a for loop to extract daily maximum temperature from AgERA5 ###
+#################################################################################################
+
+##-- Working directory --##
+
+setwd("\\\\ALLIANCEDFS.ALLIANCE.CGIAR.ORG/data_cluster17V2/Observed_Climate data/AgERA5_V2/Maximum Temperature")
+
+##################################################################################################
+## This script section will make a for loop to import the Tmax global daily maps and then clip  ##
+## them using the Zoi shapefile. Next, a folder will be created for each year to save the daily ##
+## maximum temperature maps of the study region.                                                ##
+##################################################################################################
+
+##--- Read folders with the number of years with CHIRPS information ---##
+
+years = list.dirs(getwd(), full.names = FALSE, recursive = FALSE)
+
+##--- Make the for Loop ---##
+
+for(i in 1:(length(years))){
+  #i=1
+  ##-- Import raster files for each year --##
+  
+  rastlist = list.files(path = paste(getwd(),"/",1980+i,sep =""), pattern=paste("Temperature-Air-2m_Max-24h_C3S-glob-agric_AgERA5_",1980+i,sep =""), full.names = TRUE, recursive = FALSE)
+  allrasters = terra::rast(rastlist)
+  
+  ##-- Crop and mask the Tmax maps to Zoi shapefile --##
+  
+  Tmax = crop(allrasters, Zoi)
+  
+  ##-- Convert Tmax maps from K to °C --##
+  T.max.C = (Tmax - 273.15)
+
+  ##-- resample the raster stack maps of Tmax --##
+  Tmax.5km = resample(T.max.C,reference.5km, method = 'bilinear')
+  res(Tmax.5km)==res(reference.5km)
+  
+  ##-- Create folders to save daily Tmax maps for each year --## 
+  
+  dir.create(paste("\\\\Catalogue/sat_pma/data/raw/Clima_Fuentes_Secundarias/Diario/Tmax_5km_AgERA5/",1980+i,sep ="")) #change the folder
+  
+  for(j in 1:nlyr(Tmax.5km)){
+    #j=1
+    raster.name = time(Tmax.5km)[j]
+    writeRaster(Tmax.5km[[j]],paste("\\\\Catalogue/sat_pma/data/raw/Clima_Fuentes_Secundarias/Diario/Tmax_5km_AgERA5/",1980+i,"/Tmax_",raster.name,".tif",sep =""))
+    
+  }
+  
+}
