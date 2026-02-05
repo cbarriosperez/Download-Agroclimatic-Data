49750af91416fc03a6dc8c76d82c5e430b8fa0fe
Camilo Barrios-Pérez <c.barrios@cgiar.org>
2026-02-05 14:39:16 -0500

    Download climate variables from AgERA5-v2.
    Download climate variables from AgERA5-v2.

---
+++ Script_Download_Whole region_Tmin_AgEra5_V2.py
@@ -0,0 +1,47 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+# Define the time range
+start_year = 1981
+end_year = 2025  # Adjust the end year as needed
+
+# Define the area (Global area)
+#area = [90, -180, -90, 180]
+
+# Define the variable and format
+variable = '2m_temperature'
+format_type = 'tgz'
+
+# Loop through the years and months
+for year in range(start_year, end_year + 1):
+    c.retrieve(
+        'sis-agrometeorological-indicators',
+        {
+            'version': '2_0',
+            'format': format_type,
+            'variable': variable,
+            'year': [str(year)],
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'day': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+                '13', '14', '15',
+                '16', '17', '18',
+                '19', '20', '21',
+                '22', '23', '24',
+                '25', '26', '27',
+                '28', '29', '30',
+                '31',
+            ],
+            'statistic': '24_hour_minimum',
+        },
+        f'agera5_global_tmin_{year}.tar.gz'
+    )
